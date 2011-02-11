baf9315c0cc572b7ef29471ced39075a0138a126
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2011-02-11 10:42:15 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/TaskUnifierGui@468 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- src/com/leclercb/taskunifier/gui/components/searcherlist/transfer/TaskSearcherTransferHandler.java
+++ src/com/leclercb/taskunifier/gui/components/searcherlist/transfer/TaskSearcherTransferHandler.java
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.leclercb.taskunifier.gui.components.searcherlist.draganddrop;
+package com.leclercb.taskunifier.gui.components.searcherlist.transfer;
 
 import java.awt.datatransfer.Transferable;
 
