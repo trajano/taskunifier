c7de14ab3eb2f129090373872f71318f5dce9973
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2011-02-18 15:16:37 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/TaskUnifierGui@583 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- src/com/leclercb/taskunifier/gui/actions/ActionCheckVersion.java
+++ src/com/leclercb/taskunifier/gui/actions/ActionCheckVersion.java
@@ -84,6 +84,9 @@ public class ActionCheckVersion extends AbstractAction {
 					
 					String version = EntityUtils.toString(response.getEntity()).trim();
 					
+					if (version.length() > 10)
+						throw new Exception();
+					
 					if (Constants.VERSION.compareTo(version) < 0) {
 						GuiLogger.getLogger().info(
 								"New version available : " + version);
@@ -105,12 +108,12 @@ public class ActionCheckVersion extends AbstractAction {
 									Translations.getString(
 											"action.check_version.new_version_available",
 											version),
-									Translations.getString("general.information"),
-									JOptionPane.YES_NO_OPTION,
-									JOptionPane.INFORMATION_MESSAGE,
-									null,
-									options,
-									options[0]);
+											Translations.getString("general.information"),
+											JOptionPane.YES_NO_OPTION,
+											JOptionPane.INFORMATION_MESSAGE,
+											null,
+											options,
+											options[0]);
 							
 							if (result == 0) {
 								BrowserUtils.openDefaultBrowser(Constants.DOWNLOAD_URL);
@@ -125,14 +128,14 @@ public class ActionCheckVersion extends AbstractAction {
 									Translations.getString(
 											"action.check_version.no_new_version_available",
 											Constants.VERSION),
-									Translations.getString("general.information"),
-									JOptionPane.INFORMATION_MESSAGE);
+											Translations.getString("general.information"),
+											JOptionPane.INFORMATION_MESSAGE);
 						}
 					}
 				} catch (Exception e) {
 					if (ActionCheckVersion.this.silent) {
 						GuiLogger.getLogger().warning(
-								"An error occured while checking for updates");
+						"An error occured while checking for updates");
 					} else {
 						ErrorDialog errorDialog = new ErrorDialog(
 								MainFrame.getInstance().getFrame(),
