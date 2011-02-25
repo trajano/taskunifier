6cc27038525bc66ed1f5a491243e6ce50d7afbb0
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2011-02-25 15:56:22 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/TaskUnifierGui@638 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- src/com/leclercb/taskunifier/gui/components/import_data/ImportModelsDialog.java
+++ src/com/leclercb/taskunifier/gui/components/import_data/ImportModelsDialog.java
@@ -2,11 +2,18 @@ package com.leclercb.taskunifier.gui.components.import_data;
 
 import java.awt.Frame;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipFile;
 
+import com.leclercb.taskunifier.api.models.Model;
+import com.leclercb.taskunifier.api.models.ModelId;
+import com.leclercb.taskunifier.api.models.ModelStatus;
 import com.leclercb.taskunifier.api.models.coders.ContextFactoryXMLCoder;
 import com.leclercb.taskunifier.api.models.coders.FolderFactoryXMLCoder;
 import com.leclercb.taskunifier.api.models.coders.GoalFactoryXMLCoder;
@@ -31,9 +38,13 @@ public class ImportModelsDialog extends AbstractImportDialog {
 		SynchronizerUtils.resetSynchronizerAndDeleteModels();
 	}
 	
+	@SuppressWarnings("unused")
 	@Override
 	protected void importFromFile(String file) throws Exception {
 		ZipFile zip = new ZipFile(new File(file));
+		
+		// TODO: fill in the list
+		List<Model> models = new ArrayList<Model>();
 		
 		for (Enumeration<?> e = zip.getEntries(); e.hasMoreElements();) {
 			ZipArchiveEntry entry = (ZipArchiveEntry) e.nextElement();
@@ -52,6 +63,36 @@ public class ImportModelsDialog extends AbstractImportDialog {
 			
 			if (entry.getName().equals("tasks.xml"))
 				new TaskFactoryXMLCoder().decode(zip.getInputStream(entry));
+		}
+		
+		String[] options = new String[] {
+				Translations.getString("general.yes"),
+				Translations.getString("general.no") };
+		
+		int result = -1;
+		
+		if (false) {
+			result = JOptionPane.showOptionDialog(
+					this.getOwner(),
+					"Flag all the imported data as new data ? (Allow you to sync/add them with/to another service provider)",
+					Translations.getString("general.question"),
+					JOptionPane.YES_NO_OPTION,
+					JOptionPane.QUESTION_MESSAGE,
+					null,
+					options,
+					options[1]);
+		}
+		
+		if (result == 0) {
+			for (Model model : models) {
+				if (model.getModelStatus() == ModelStatus.LOADED
+						|| model.getModelStatus() == ModelStatus.TO_UPDATE) {
+					// TODO: won't work because cannot replace new id by
+					// existing id
+					model.setModelId(new ModelId());
+					model.setModelStatus(ModelStatus.TO_UPDATE);
+				}
+			}
 		}
 	}
 	
