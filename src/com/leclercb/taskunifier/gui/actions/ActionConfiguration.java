0fc38daea96f4870af9e70d7f695034dcc821933
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2011-05-04 15:30:29 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/TaskUnifierGui@951 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- src/com/leclercb/taskunifier/gui/actions/ActionConfiguration.java
+++ src/com/leclercb/taskunifier/gui/actions/ActionConfiguration.java
@@ -37,7 +37,6 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 
 import com.leclercb.taskunifier.gui.components.configuration.ConfigurationDialog;
-import com.leclercb.taskunifier.gui.main.MainFrame;
 import com.leclercb.taskunifier.gui.translations.Translations;
 import com.leclercb.taskunifier.gui.utils.Images;
 
@@ -63,9 +62,7 @@ public class ActionConfiguration extends AbstractAction {
 	}
 	
 	public static void configuration() {
-		ConfigurationDialog config = new ConfigurationDialog(
-				MainFrame.getInstance().getFrame(),
-				true);
+		ConfigurationDialog config = ConfigurationDialog.getInstance();
 		config.setVisible(true);
 	}
 	
