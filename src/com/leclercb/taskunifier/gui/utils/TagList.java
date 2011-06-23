3cce80ac8a194ef6b80a84884f8d839821d96ae9
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2011-06-23 11:39:26 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/TaskUnifierGui@1258 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- src/com/leclercb/taskunifier/gui/utils/TagList.java
+++ src/com/leclercb/taskunifier/gui/utils/TagList.java
@@ -1,22 +1,45 @@
 package com.leclercb.taskunifier.gui.utils;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.leclercb.commons.api.event.propertychange.PropertyChangeSupport;
+import com.leclercb.commons.api.event.propertychange.PropertyChangeSupported;
 import com.leclercb.commons.api.utils.IgnoreCaseString;
 import com.leclercb.taskunifier.api.models.ModelStatus;
 import com.leclercb.taskunifier.api.models.Task;
 import com.leclercb.taskunifier.api.models.TaskFactory;
 
-public final class TagList {
+public final class TagList implements PropertyChangeSupported, PropertyChangeListener {
 	
-	private Set<IgnoreCaseString> tags;
+	public static final String PROP_TAG_ADDED = "PROP_TAG_ADDED";
+	public static final String PROP_TAG_REMOVED = "PROP_TAG_REMOVED";
+	
+	private PropertyChangeSupport propertyChangeSupport;
+	
+	private List<IgnoreCaseString> tags;
 	
 	private TagList() {
-		this.tags = new HashSet<IgnoreCaseString>();
+		this.propertyChangeSupport = new PropertyChangeSupport(this);
+		this.tags = new ArrayList<IgnoreCaseString>();
+		
 		this.initialize();
+	}
+	
+	public String[] getTags() {
+		Set<IgnoreCaseString> uniqueTags = new HashSet<IgnoreCaseString>(
+				this.tags);
+		List<IgnoreCaseString> tags = new ArrayList<IgnoreCaseString>(
+				uniqueTags);
+		
+		// tags = Collections.sort(tags);
+		
+		return null;
 	}
 	
 	private void initialize() {
@@ -30,6 +53,62 @@ public final class TagList {
 			
 			this.tags.addAll(Arrays.asList(IgnoreCaseString.as(task.getTags())));
 		}
+		
+		TaskFactory.getInstance().addPropertyChangeListener(
+				Task.PROP_TAGS,
+				this);
+	}
+	
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		List<IgnoreCaseString> tags = null;
+		
+		tags = Arrays.asList(IgnoreCaseString.as((String[]) evt.getOldValue()));
+		this.tags.removeAll(tags);
+		
+		for (IgnoreCaseString tag : tags) {
+			if (!this.tags.contains(tag))
+				this.propertyChangeSupport.firePropertyChange(
+						PROP_TAG_REMOVED,
+						tag.toString(),
+						tag.toString());
+		}
+		
+		tags = Arrays.asList(IgnoreCaseString.as((String[]) evt.getNewValue()));
+		
+		List<IgnoreCaseString> newTags = new ArrayList<IgnoreCaseString>();
+		for (IgnoreCaseString tag : tags) {
+			if (!this.tags.contains(tag))
+				newTags.add(tag);
+		}
+		
+		this.tags.addAll(tags);
+		
+		for (IgnoreCaseString tag : newTags) {
+			this.propertyChangeSupport.firePropertyChange(
+					PROP_TAG_ADDED,
+					tag.toString(),
+					tag.toString());
+		}
+	}
+	
+	@Override
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		this.propertyChangeSupport.addPropertyChangeListener(listener);
+	}
+	
+	@Override
+	public void addPropertyChangeListener(
+			String propertyName,
+			PropertyChangeListener listener) {
+		this.propertyChangeSupport.addPropertyChangeListener(
+				propertyName,
+				listener);
+	}
+	
+	@Override
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		this.propertyChangeSupport.removePropertyChangeListener(listener);
 	}
 	
 }
