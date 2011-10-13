8850a123fd2395681c3c08a76dfc5eb8a2787bf4
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2011-10-13 09:01:20 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/TaskUnifierGui@1872 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

---
+++ src/com/leclercb/taskunifier/gui/threads/communicator/CommunicatorThread.java
@@ -0,0 +1,36 @@
+package com.leclercb.taskunifier.gui.threads.communicator;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import com.leclercb.commons.gui.logger.GuiLogger;
+import com.leclercb.taskunifier.gui.main.Main;
+
+public class CommunicatorThread extends Thread {
+	
+	public CommunicatorThread() {
+		
+	}
+	
+	@Override
+	public void run() {
+		ServerSocket serverSocket = null;
+		
+		try {
+			int port = Main.SETTINGS.getIntegerProperty("general.communicator.port");
+			serverSocket = new ServerSocket(port);
+			
+			while (true)
+				new CommunicatorClient(serverSocket.accept()).start();
+		} catch (Exception e) {
+			GuiLogger.getLogger().warning("Cannot initialize communicator");
+		} finally {
+			try {
+				serverSocket.close();
+			} catch (IOException e) {
+				GuiLogger.getLogger().warning("Cannot close communicator");
+			}
+		}
+	}
+	
+}
