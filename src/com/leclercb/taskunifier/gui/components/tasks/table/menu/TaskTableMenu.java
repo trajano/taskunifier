53c9225f28ab1cdefe0166a94d88f07ba56e8868
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2011-11-21 14:10:01 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/TaskUnifierGui@2092 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- src/com/leclercb/taskunifier/gui/components/tasks/table/menu/TaskTableMenu.java
+++ src/com/leclercb/taskunifier/gui/components/tasks/table/menu/TaskTableMenu.java
@@ -40,6 +40,7 @@ import javax.swing.JPopupMenu;
 
 import com.leclercb.commons.api.utils.CheckUtils;
 import com.leclercb.taskunifier.gui.actions.ActionAddSubTask;
+import com.leclercb.taskunifier.gui.actions.ActionAddSubTaskAtSameLevel;
 import com.leclercb.taskunifier.gui.actions.ActionAddTask;
 import com.leclercb.taskunifier.gui.actions.ActionCollapseAll;
 import com.leclercb.taskunifier.gui.actions.ActionDelete;
@@ -72,6 +73,7 @@ public class TaskTableMenu extends JPopupMenu {
 		this.addSeparator();
 		this.add(new ActionAddTask(16, 16));
 		this.add(new ActionAddSubTask(16, 16));
+		this.add(new ActionAddSubTaskAtSameLevel(16, 16));
 		this.add(new ActionDuplicateTasks(16, 16));
 		this.addSeparator();
 		this.add(new ActionDelete(16, 16));
