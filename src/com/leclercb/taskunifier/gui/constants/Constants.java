35914cd655589e14d70fc979fd9432f7b282b110
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2012-01-17 19:59:15 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/TaskUnifierGui@2341 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- src/com/leclercb/taskunifier/gui/constants/Constants.java
+++ src/com/leclercb/taskunifier/gui/constants/Constants.java
@@ -71,7 +71,7 @@ public final class Constants {
 	public static final String TITLE = "TaskUnifier";
 	public static final String VERSION = "2.0.0";
 	
-	public static final int PLUGIN_API_VERSION = 15;
+	public static final int PLUGIN_API_VERSION = 16;
 	public static final int WAIT_NO_LICENSE_TIME = 30;
 	public static final int WAIT_NO_LICENSE_ADDED_TIME = 15;
 	
