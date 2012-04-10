ebb3cae9413d22fae539da21c980a4701b202d86
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2012-04-10 10:56:15 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/branches/3_0_0@2688 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- TaskUnifierGui/src/com/leclercb/taskunifier/gui/components/configuration/SynchronizationConfigurationPanel.java
+++ TaskUnifierGui/src/com/leclercb/taskunifier/gui/components/configuration/SynchronizationConfigurationPanel.java
@@ -42,7 +42,19 @@ import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
+import com.leclercb.taskunifier.api.models.Contact;
+import com.leclercb.taskunifier.api.models.ContactFactory;
+import com.leclercb.taskunifier.api.models.Context;
+import com.leclercb.taskunifier.api.models.ContextFactory;
+import com.leclercb.taskunifier.api.models.Folder;
+import com.leclercb.taskunifier.api.models.FolderFactory;
+import com.leclercb.taskunifier.api.models.Goal;
+import com.leclercb.taskunifier.api.models.GoalFactory;
+import com.leclercb.taskunifier.api.models.Location;
+import com.leclercb.taskunifier.api.models.LocationFactory;
 import com.leclercb.taskunifier.api.models.ModelStatus;
+import com.leclercb.taskunifier.api.models.Note;
+import com.leclercb.taskunifier.api.models.NoteFactory;
 import com.leclercb.taskunifier.api.models.Task;
 import com.leclercb.taskunifier.api.models.TaskFactory;
 import com.leclercb.taskunifier.gui.actions.ActionManageSynchronizerPlugins;
@@ -243,6 +255,42 @@ public class SynchronizationConfigurationPanel extends DefaultConfigurationPanel
 				public void actionPerformed(ActionEvent event) {
 					if (SynchronizationConfigurationPanel.this.getConfigurationGroup() != null) {
 						SynchronizationConfigurationPanel.this.getConfigurationGroup().saveAndApplyConfig();
+					}
+					
+					List<Contact> contacts = ContactFactory.getInstance().getList();
+					for (Contact contact : contacts) {
+						if (contact.getModelStatus().isEndUserStatus())
+							contact.setModelStatus(ModelStatus.TO_UPDATE);
+					}
+					
+					List<Context> contexts = ContextFactory.getInstance().getList();
+					for (Context context : contexts) {
+						if (context.getModelStatus().isEndUserStatus())
+							context.setModelStatus(ModelStatus.TO_UPDATE);
+					}
+					
+					List<Folder> folders = FolderFactory.getInstance().getList();
+					for (Folder folder : folders) {
+						if (folder.getModelStatus().isEndUserStatus())
+							folder.setModelStatus(ModelStatus.TO_UPDATE);
+					}
+					
+					List<Goal> goals = GoalFactory.getInstance().getList();
+					for (Goal goal : goals) {
+						if (goal.getModelStatus().isEndUserStatus())
+							goal.setModelStatus(ModelStatus.TO_UPDATE);
+					}
+					
+					List<Location> locations = LocationFactory.getInstance().getList();
+					for (Location location : locations) {
+						if (location.getModelStatus().isEndUserStatus())
+							location.setModelStatus(ModelStatus.TO_UPDATE);
+					}
+					
+					List<Note> notes = NoteFactory.getInstance().getList();
+					for (Note note : notes) {
+						if (note.getModelStatus().isEndUserStatus())
+							note.setModelStatus(ModelStatus.TO_UPDATE);
 					}
 					
 					List<Task> tasks = TaskFactory.getInstance().getList();
