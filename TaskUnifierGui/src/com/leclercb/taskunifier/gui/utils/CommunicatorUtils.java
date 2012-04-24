86fe1ab0b7421a21dfdda943639c87e800f4aaf0
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2012-04-24 15:00:24 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/branches/3_0_0@2731 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- TaskUnifierGui/src/com/leclercb/taskunifier/gui/utils/CommunicatorUtils.java
+++ TaskUnifierGui/src/com/leclercb/taskunifier/gui/utils/CommunicatorUtils.java
@@ -32,8 +32,6 @@
  */
 package com.leclercb.taskunifier.gui.utils;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
 import java.net.Socket;
 
 import com.leclercb.taskunifier.gui.api.models.beans.ComBean;
@@ -46,17 +44,15 @@ public final class CommunicatorUtils {
 	
 	public static void send(ComBean bean, String host, int port)
 			throws Exception {
-		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		bean.encodeToXML(output);
-		output.close();
+		Socket socket = null;
 		
-		Socket socket = new Socket(host, port);
-		BufferedOutputStream stream = new BufferedOutputStream(
-				socket.getOutputStream());
-		stream.write(output.toByteArray());
-		
-		stream.close();
-		socket.close();
+		try {
+			socket = new Socket(host, port);
+			bean.encodeToXML(socket.getOutputStream());
+		} finally {
+			if (socket != null)
+				socket.close();
+		}
 	}
 	
 }
