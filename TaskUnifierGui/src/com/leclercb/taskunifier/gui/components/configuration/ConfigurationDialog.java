3c08fdd19393c3101b4130f3260ee62e92006871
leclercb <leclercb@4cc7d151-2d3f-4153-82be-2c8b9e7ce797>
2012-09-17 19:14:02 +0000

    git-svn-id: https://taskunifier.svn.sourceforge.net/svnroot/taskunifier/branches/3_0_0@2839 4cc7d151-2d3f-4153-82be-2c8b9e7ce797

--- TaskUnifierGui/src/com/leclercb/taskunifier/gui/components/configuration/ConfigurationDialog.java
+++ TaskUnifierGui/src/com/leclercb/taskunifier/gui/components/configuration/ConfigurationDialog.java
@@ -33,15 +33,19 @@
 package com.leclercb.taskunifier.gui.components.configuration;
 
 import java.awt.BorderLayout;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.logging.Level;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.KeyStroke;
 
 import org.jdesktop.swingx.JXErrorPane;
 import org.jdesktop.swingx.JXHeader;
@@ -163,35 +167,55 @@ public class ConfigurationDialog extends JDialog implements ConfigurationGroup, 
 	}
 	
 	private void initializeButtonsPanel() {
-		ActionListener listener = new ActionListener() {
+		ActionListener okListener = new ActionListener() {
 			
 			@Override
 			public void actionPerformed(ActionEvent event) {
-				if (event.getActionCommand().equals("OK")) {
-					ConfigurationDialog.this.saveAndApplyConfig();
-					ConfigurationDialog.this.setVisible(false);
-				}
-				
-				if (event.getActionCommand().equals("CANCEL")) {
-					ConfigurationDialog.this.cancelConfig();
-					ConfigurationDialog.this.setVisible(false);
-				}
-				
-				if (event.getActionCommand().equals("APPLY")) {
-					ConfigurationDialog.this.saveAndApplyConfig();
-				}
+				ConfigurationDialog.this.saveAndApplyConfig();
+				ConfigurationDialog.this.setVisible(false);
 			}
 			
 		};
 		
-		JButton okButton = new TUOkButton(listener);
-		JButton cancelButton = new TUCancelButton(listener);
-		JButton applyButton = new TUApplyButton(listener);
+		ActionListener applyListener = new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				ConfigurationDialog.this.saveAndApplyConfig();
+			}
+			
+		};
+		
+		ActionListener cancelListener = new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				ConfigurationDialog.this.cancelConfig();
+				ConfigurationDialog.this.setVisible(false);
+			}
+			
+		};
+		
+		JButton okButton = new TUOkButton(okListener);
+		JButton cancelButton = new TUCancelButton(cancelListener);
+		JButton applyButton = new TUApplyButton(applyListener);
 		
 		JPanel panel = new TUButtonsPanel(okButton, cancelButton, applyButton);
 		
 		this.add(panel, BorderLayout.SOUTH);
 		this.getRootPane().setDefaultButton(okButton);
+		
+		this.getRootPane().registerKeyboardAction(
+				okListener,
+				KeyStroke.getKeyStroke(
+						KeyEvent.VK_S,
+						Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()),
+				JComponent.WHEN_IN_FOCUSED_WINDOW);
+		
+		this.getRootPane().registerKeyboardAction(
+				cancelListener,
+				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+				JComponent.WHEN_IN_FOCUSED_WINDOW);
 	}
 	
 	private void initializeGeneralPanel() {
